3e9a1e5139a6c34df9a43d28ac0138b070b0a389
juliogodoy <34786006+juliogodoy@users.noreply.github.com>
2023-07-20 23:29:49 -0400

    example upload
    example uploaded

---
+++ build/RVO2/examples/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/julio/Desktop/Python-RVO2-main/examples
+# Build directory: /home/julio/Desktop/Python-RVO2-main/build/RVO2/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Blocks "Blocks")
+add_test(Circle "Circle")
+add_test(Roadmap "Roadmap")
